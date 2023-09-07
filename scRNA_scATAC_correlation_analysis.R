b73c2dd5d2d399b8f4616031a9b340cc5e771f84
kourosh.zarringhalam <kourosh.zarringhalam@CSMxUS00002114.local>
2023-09-07 19:35:01 -0400

    1238 used for ccs

--- scRNA_scATAC_correlation_analysis.R
+++ scRNA_scATAC_correlation_analysis.R
@@ -28,8 +28,6 @@ sc.atac.spline.fits <- readRDS('../Input_sub/toxo_cdc/rds_ME49_59/sc_atac_spline
 # cyclic.genes <- readRDS('../Input_sub/toxo_cdc/rds_ME49_59/all_genes_cyclic_timing.rds')
 # cyclic.genes <- cyclic.genes %>% dplyr::filter(rna.cyclic == 1 & atac.cyclic == 1)
 
-cyclic.genes <- read.xlsx('../OutPut/toxo_cdc/ME49_59/tables/all_genes_cyclic_timing_KZ.xlsx')
-cyclic.genes <- cyclic.genes %>% dplyr::filter(rna.cyclic == 1 & atac.cyclic == 1)
 
 marker.genes <- readRDS('../Input_sub/toxo_cdc/rds_ME49_59/Intra_markers_sig.rds')
 marker.genes.phase <- marker.genes %>% transmute(GeneID = gene, phase = cluster) %>% distinct()
